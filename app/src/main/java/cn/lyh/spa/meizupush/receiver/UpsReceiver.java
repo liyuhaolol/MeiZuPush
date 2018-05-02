61d474f8c32475aa1a290655e43e1e5df51812b8
liyuhao <liyuhaoid@sina.com>
2018-05-02 16:20:51 +0800

    集成华为，魅族，小米推送，在魅族平台

--- app/src/main/java/cn/lyh/spa/meizupush/receiver/UpsReceiver.java
+++ app/src/main/java/cn/lyh/spa/meizupush/receiver/UpsReceiver.java
@@ -1,4 +1,34 @@
 package cn.lyh.spa.meizupush.receiver;
 
-public class UpsReceiver {
+import android.content.Context;
+
+import com.meizu.upspushsdklib.UpsCommandMessage;
+import com.meizu.upspushsdklib.UpsPushMessage;
+import com.meizu.upspushsdklib.UpsPushMessageReceiver;
+
+public class UpsReceiver extends UpsPushMessageReceiver{
+    @Override
+    public void onThroughMessage(Context context, UpsPushMessage upsPushMessage) {
+
+    }
+
+    @Override
+    public void onNotificationClicked(Context context, UpsPushMessage upsPushMessage) {
+
+    }
+
+    @Override
+    public void onNotificationArrived(Context context, UpsPushMessage upsPushMessage) {
+
+    }
+
+    @Override
+    public void onNotificationDeleted(Context context, UpsPushMessage upsPushMessage) {
+
+    }
+
+    @Override
+    public void onUpsCommandResult(Context context, UpsCommandMessage upsCommandMessage) {
+
+    }
 }
