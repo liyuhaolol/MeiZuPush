35c3a712f69e65cfcf032ee69dfb1d1000005782
MuteClimber <zhaolibo_000@163.com>
2018-05-03 09:38:22 +0800

    添加push注册

---
+++ app/src/main/java/cn/lyh/spa/meizupush/App.java
@@ -0,0 +1,18 @@
+package cn.lyh.spa.meizupush;
+
+import android.app.Application;
+
+import com.meizu.upspushsdklib.UpsPushManager;
+
+/**
+ * Created by zhaolb on 2018/5/3.
+ */
+
+public class App extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        UpsPushManager.register(getApplicationContext(),"1000357","e7e0e4ea5ce04fab8b83695059364983");
+    }
+}
