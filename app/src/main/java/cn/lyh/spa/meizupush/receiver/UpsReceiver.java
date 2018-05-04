0d68286651d8c231da209bfe97c89ba302a06da7
liyuhao <liyuhaoid@sina.com>
2018-05-04 16:29:10 +0800

    集成华为，魅族，小米推送，在魅族平台

--- app/src/main/java/cn/lyh/spa/meizupush/receiver/UpsReceiver.java
+++ app/src/main/java/cn/lyh/spa/meizupush/receiver/UpsReceiver.java
@@ -1,6 +1,8 @@
 package cn.lyh.spa.meizupush.receiver;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.meizu.upspushsdklib.UpsCommandMessage;
 import com.meizu.upspushsdklib.UpsPushMessage;
@@ -29,6 +31,28 @@ public class UpsReceiver extends UpsPushMessageReceiver{
 
     @Override
     public void onUpsCommandResult(Context context, UpsCommandMessage upsCommandMessage) {
-
+       switch (upsCommandMessage.getCommandType()){
+           case REGISTER://注册
+               SharedPreferences sp = context.getSharedPreferences("pushInfo",Context.MODE_PRIVATE);
+               SharedPreferences.Editor ed = sp.edit();
+               String pushid = sp.getString("pushid","");
+               if (!pushid.equals(upsCommandMessage.getCommandResult())){
+                   //如果是个新的pushid
+                   //保存pushid
+                   ed.putString("pushid",upsCommandMessage.getCommandResult());
+                   ed.apply();
+                   //执行对应的网络请求
+                   Log.e("liyuhao",upsCommandMessage.getCommandResult());
+               }
+               break;
+           case UNREGISTER://反注册
+               break;
+           case SUBALIAS://设置别名
+               break;
+           case UNSUBALIAS://反注册别名
+               break;
+           case ERROR://有错误
+               break;
+        }
     }
 }
